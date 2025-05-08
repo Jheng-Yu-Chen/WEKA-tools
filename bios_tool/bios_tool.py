1ba388bdca44800eda7b2092f3404e77439c9388
aaron chen <new.f@livemail.tw>
2025-05-08 23:58:19 +0800

    20250508

--- bios_tool/bios_tool.py
+++ bios_tool/bios_tool.py
@@ -2,7 +2,6 @@ import argparse
 import logging
 import sys
 import traceback
-
 import redfish
 import yaml
 
@@ -11,278 +10,98 @@ from RedFishBMC import RedFishBMC, trim_supermicro_dict
 from BMCsetup import bmc_setup
 from tabulate import tabulate
 
-# from paramiko.util import log_to_file
-
-# get root logger
 log = logging.getLogger()
 
 def csv_load(f):
-    """
-    Load a CSV file into a list of dictionaries
-    :param f: file object
-    :return: list of dictionaries
-    """
     import csv
     reader = csv.DictReader(f)
-    #reader_list = list(reader)
-    #for row in reader:
-    #    print(row)
-    return {"hosts":list(reader)}
+    return {"hosts": list(reader)}
 
+def _load_config(inputfile):
+    with open(inputfile) as f:
+        if inputfile.endswith(".csv"):
+            return csv_load(f)
+        return yaml.load(f, Loader=yaml.FullLoader)
 
-# load a config file - either CSV or YAML
-def load_config(inputfile):
-    try:
-        f = open(inputfile)
-    except Exception as exc:
-        raise
-    with f:
-        try:
-            if inputfile.endswith(".csv"):
-                return csv_load(f)
-            return yaml.load(f, Loader=yaml.FullLoader)
-        except AttributeError:
-            return yaml.load(f)
-        except Exception as exc:
-            log.error(f"Error reading config file: {exc}")
-            raise
-
-def generate_config(bmc_ips, bmc_username, bmc_password):
-    conf = dict()
-    conf['hosts'] = list()
-    for ip in bmc_ips:
-        conf['hosts'].append({'name': ip, 'user': bmc_username[0], 'password': bmc_password[0]})
-    return conf
-
-
-def bios_diff(hostlist):
-    hosta = hostlist[0]
-    hostb = hostlist[1]
-
-    if hosta.arch != hostb.arch:
-        print()
-        print(f"ERROR: Hosts are of different architectures! {hosta.arch} vs {hostb.arch}")
-        sys.exit(1)
-    if hosta.vendor != hostb.vendor:
-        print()
-        print(f"ERROR: Hosts are of different vendors! {hosta.vendor} vs {hostb.vendor}")
-        sys.exit(1)
-    if hosta.bios_version != hostb.bios_version:
-        print()
-        print(f"WARNING: Hosts have different BIOS versions! {hosta.bios_version} vs {hostb.bios_version}")
-        # note:  Supermicro has a different BIOS keys in every bios version (all end in _XXXX, where XXXX is hex)
-        # the XXXX is different in different versions.  This means that the BIOS settings are not comparable
-        # between different versions of the BIOS.
-        # Perhaps we should trim the keys to remove the _XXXX part so we can compare different BIOS versions?
-        # This could be implemented by copying the hosta_bios and hostb_bios dictionaries and removing the _XXXX
-        # from the keys.  Then we could compare the two dictionaries.
-
-    if hosta.vendor == "Supermicro":
-        hosta_bios = trim_supermicro_dict(hostlist[0].bios_data.dict['Attributes'])
-        hostb_bios = trim_supermicro_dict(hostlist[1].bios_data.dict['Attributes'])
-    else:
-        hosta_bios = hostlist[0].bios_data.dict['Attributes']
-        hostb_bios = hostlist[1].bios_data.dict['Attributes']
-
-    diff = list()
-    settings_not_present = list()
-    for setting, value in hosta_bios.items():
-        if setting not in hostb_bios:
-            settings_not_present.append([setting, value, "setting not present"])
-        elif hostb_bios[setting] != value:
-            diff.append([setting, value, hostb_bios[setting]])
-
-    # check for settings in b that are not in a
-    for setting, value in hostb_bios.items():
-        if setting not in hosta_bios:
-            settings_not_present.append([setting, "setting not present", value])
-
-    are_different = False
-    if len(diff) > 0:
-        print()
-        print("Settings that are different between the servers:")
-        print(tabulate(diff, headers=["Setting", hosta.name, hostb.name]))
-        are_different = True
-
-    if len(settings_not_present) > 0:
-        print()
-        print("Settings that are in one server and not the other:")
-        print(tabulate(settings_not_present, headers=["Setting", hosta.name, hostb.name]))
-        are_different = True
-
-    return are_different
-
-def open_sessions(hostlist):
-    # open connections to all the hosts
-    redfish_list = list()
-    for host in hostlist:
-        log.info(f"Fetching BIOS settings of host {host['name']}")
-        try:
-            redfish_list.append(RedFishBMC(host['name'], username=host['user'], password=host['password']))
-        except redfish.rest.v1.InvalidCredentialsError:
-            continue
-        except Exception as exc:
-            log.error(f"Error opening connections to {host['name']}: {exc}")
-            print(traceback.format_exc())
-    return(redfish_list)
-
-def close_sessions(hostlist):
-    for host in hostlist:
-        host.redfish.logout()
+def _generate_config(bmc_ips, bmc_username, bmc_password):
+    return {
+        "hosts": [
+            {"name": ip, "user": bmc_username[0], "password": bmc_password[0]}
+            for ip in bmc_ips
+        ]
+    }
 
 def main():
-    # parse arguments
-    progname = sys.argv[0]
     parser = argparse.ArgumentParser(description="View/Change BIOS Settings on servers")
-
-    # parser.add_argument("host", type=str, nargs="?", help="a host to talk to", default="localhost")
-    parser.add_argument("-c", "--hostconfigfile", type=str, nargs="?", help="filename of host config file",
-                        default="host_config.csv")
-    parser.add_argument("-b", "--bios", type=str, nargs="?", help="bios configuration filename",
-                        default="bios_settings.yml")
-    parser.add_argument("--bmc_config", dest="bmc_config", default=False, action="store_true",
-                        help="Configure the BMCs to allow RedFish access")
-    parser.add_argument("--fix", dest="fix", default=False, action="store_true",
-                        help="Correct any bios settings that do not match the definition")
-    parser.add_argument("--reboot", dest="reboot", default=False, action="store_true",
-                        help="Reboot server if changes have been made")
-    parser.add_argument("--dump", dest="dump", default=False, action="store_true",
-                        help="Print out BIOS settings only")
-    parser.add_argument("--reset_bios", dest="reset_bios", default=False, action="store_true",
-                        help="Reset BIOS to default settings.  To also reboot, add the --reboot option")
-    parser.add_argument("--diff", dest="diff", nargs=2, default=False, help="Compare 2 hosts BIOS settings")
-    # parser.add_argument("--version", dest="version", default=False, action="store_true",
-    #                    help="Display version number")
-    parser.add_argument("--bmc_ips", dest="bmc_ips", type=str, nargs="*",
-                        help="a list of hosts to configure, or none to use cluster beacons", default=None)
-    parser.add_argument("--bmc_username", dest="bmc_username", type=str, nargs=1,
-                        help="a username to use on all hosts in --bmc_ips", default=None)
-    parser.add_argument("--bmc_password", dest="bmc_password", type=str, nargs=1,
-                        help="a password to use on all hosts in --bmc_ips", default=None)
-    parser.add_argument("-v", "--verbose", dest='verbosity', action='store_true', help="enable verbose mode")
-    parser.add_argument("--version", dest='version', action='store_true', help="report program version and exit")
+    parser.add_argument("-c", "--hostconfigfile", type=str, default="host_config.csv")
+    parser.add_argument("-b", "--bios", type=str, default="bios_settings.yml")
+    parser.add_argument("--arch", choices=["Intel", "AMD"], help="Override CPU architecture detection")
+    parser.add_argument("--bmc_config", action="store_true")
+    parser.add_argument("--fix", action="store_true")
+    parser.add_argument("--reboot", action="store_true")
+    parser.add_argument("--dump", action="store_true")
+    parser.add_argument("--reset_bios", action="store_true")
+    parser.add_argument("--diff", nargs=2, default=False)
+    parser.add_argument("--bmc_ips", type=str, nargs="*", default=None)
+    parser.add_argument("--bmc_username", type=str, nargs=1, default=None)
+    parser.add_argument("--bmc_password", type=str, nargs=1, default=None)
+    parser.add_argument("-v", "--verbose", dest='verbosity', action='store_true')
+    parser.add_argument("--version", action='store_true')
 
     args = parser.parse_args()
 
     if args.version:
-        print(f"{progname} version 2025.03.14")
+        print("bios_tool.py version 2024.08.15")
         sys.exit(0)
 
-    # local modules - override a module's logging level
     register_module("RedFishBMC", logging.INFO)
-    register_module("BMCsetup", logging.INFO)
-    register_module("redfish.rest.v1", logging.ERROR)
-    register_module("paramiko", logging.ERROR)
-
-    # set up logging in a standard way...
     configure_logging(log, args.verbosity)
-    #log_to_file("paramiko.log", logging.DEBUG)
 
-
-    # if they provided a list of BMC IPs, they must also provide a username and password
-    if args.bmc_ips is not None:
+    if args.bmc_ips:
         if args.bmc_username is None or args.bmc_password is None:
             log.error("You must provide a username and password when using --bmc_ips")
             sys.exit(1)
-        log.info(f"Using BMC IPs - ignoring {args.hostconfigfile}")
-        conf = generate_config(args.bmc_ips, args.bmc_username, args.bmc_password)
+        conf = _generate_config(args.bmc_ips, args.bmc_username, args.bmc_password)
     else:
-        # else try to load the configfile
         try:
-            conf = load_config(args.hostconfigfile)
+            conf = _load_config(args.hostconfigfile)
         except Exception as exc:
             log.error(f"Unable to open host configuration file: {exc}")
             sys.exit(1)
 
-    # did the user ask us to make sure the BMC is set with ipmi over lan and redfish, etc?
-    if args.bmc_config:
-        log.info("Configuring BMCs")
-        for host in conf['hosts']:
-            log.info(f"Configuring {host['name']}")
-            bmc_setup(host['name'], host['user'], host['password'])
-        log.info("BMCs have been configured")
-        sys.exit(0)
-
-    # try to load the BIOS settings
     try:
-        desired_bios_settings = load_config(args.bios)
+        desired_bios_settings = _load_config(args.bios)
     except Exception as exc:
         log.error(f"Unable to parse bios settings configuration file: {exc}")
         sys.exit(1)
 
-    # if they asked for a diff of two servers, make sure they're in the config file...
-    if args.diff:
-        hostlist = list()
-        for c_host in args.diff:
-            try:
-                hostlist.append([x for x in conf['hosts'] if x['name'] == c_host][0])
-            except:
-                log.error(f"host {c_host} not in {args.hostconfigfile}")
-                sys.exit(1)
-    else:
-        # all hosts
-        hostlist = conf['hosts']
+    hostlist = conf["hosts"] if not args.diff else [x for x in conf["hosts"] if x["name"] in args.diff]
+    redfish_list = []
 
-    # open connections to all the hosts - redfish_list is a list of RedFishBMC objects
-    redfish_list = open_sessions(hostlist)
+    for host in hostlist:
+        try:
+            redfish_list.append(RedFishBMC(host["name"], username=host["user"], password=host["password"], arch_override=args.arch))
+        except Exception as exc:
+            log.error(f"Error opening connections to {host['name']}: {exc}")
+            print(traceback.format_exc())
 
-    if args.diff:
-        if len(redfish_list) != 2:
-            log.error(f"hostlist has too few members to continue")
-        elif not bios_diff(redfish_list):
-            log.info("The servers have identical BIOS settings")
-    elif args.reset_bios:
-        for bmc in redfish_list:
-            # rest the bios...
+    for bmc in redfish_list:
+        if args.dump:
+            bmc.print_settings()
+            continue
+        elif args.reset_bios:
             bmc.reset_settings_to_default()
-            log.info(f"{bmc.name} has been reset to factory defaults")
             if args.reboot:
                 bmc.reboot()
-                log.info(f"{bmc.name} has been rebooted")
-    else:
-        # check BIOS settings
-        hosts_needing_changes = list()
-        fixed_hosts = list()
-        systems_rebooted = list()
-        for bmc in redfish_list:
-            if args.dump:
-                bmc.print_settings()
-                continue
-            else:
-                count = bmc.check_settings(desired_bios_settings[bmc.vendor][bmc.arch])
-            log.info(f"")
-            if count > 0:
-                log.warning(f"{count} changes are needed on {bmc.name}")
-                hosts_needing_changes.append(bmc)
-                if args.fix:
-                    if bmc.change_settings(desired_bios_settings[bmc.vendor][bmc.arch]):
-                        fixed_hosts.append(bmc)
-                    else:
-                        log.error(f"Unable to fix {bmc.name}")
-            else:
-                log.warning(f"No changes are needed on {bmc.name}")
-            log.info("")
-            # if they said reboot, reboot
-            if args.reboot:
-                if args.fix and bmc in fixed_hosts: # --fix --reboot implies rebooting only the hosts that were fixed
-                    log.info(f"Rebooting {bmc.name}")
-                    bmc.reboot()
-                    systems_rebooted.append(bmc)
-                elif not args.fix:  # they asked to reboot all hosts
-                    log.info(f"Rebooting {bmc.name}")
-                    bmc.reboot()
-                    systems_rebooted.append(bmc)
-
-        if not args.fix:
-            log.info(f"There are {len(hosts_needing_changes)} hosts needing changes")
         else:
-            if not args.reboot:
-                log.info(f"{len(fixed_hosts)} have been modified.  Please reboot them to activate changes.")
-            else:
-                log.info(f"{len(systems_rebooted)} have been successfully modified and rebooted.")
+            count = bmc.check_settings(desired_bios_settings[bmc.vendor][bmc.arch])
+            if count > 0 and args.fix:
+                bmc.change_settings(desired_bios_settings[bmc.vendor][bmc.arch])
+                if args.reboot:
+                    bmc.reboot()
 
-    close_sessions(redfish_list)
+    for bmc in redfish_list:
+        bmc.redfish.logout()
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- preinstall/dual-port-routing-setup.sh
+++ preinstall/dual-port-routing-setup.sh
@@ -1,131 +1,193 @@
-#!/bin/bash
+#!/usr/bin/env bash 
 
-# Option/argument processing configuration
-print_usage_on_arg_err=0
-raw_mode_support=0
+#
+# Set up dual-ported routes
+#
 
-# For multi-line error messages, e.g. this style of output, where "example.sh"
-# is $0:
-# example.sh: Error encountered
-#             More error info here
-error_indent=$(for i in $(seq 1 "${#0}"); do printf ' '; done)
+errmsg()
+{
+    echo $* >&2
+} # errmsg
 
-script_name=$(basename "$0")
+network_spec()
+{
+    # input arg is a ip addr spec in the form ipaddr/maskbits - X.X.X.X/nn
+    # returns network number spec in the form X.X.X.X/nn
 
-usage() {
-	cat <<- EOF
-	Usage: $script_name [ -e | -i ] PORT [ PORT ... ]
+    #IP=`echo $1 | awk -F/ '{print $1}'`
+    #MASKBITS=`echo $1 | awk -F/ '{print $2}'`
 
-	Sets PCI_WR_ORDERING (and ADVANCED_PCI_SETTINGS) for Mellanox cards. Multiple
-	ports can be specified.
+    # sorry, but this is way easier in Python...
+    python3 -c '
+import argparse
 
-	Options:
-	    -h, --help
-	        Show this help/usage message.
-	EOF
-}
+# Parse arguments
+parser = argparse.ArgumentParser(description="get ip information")
+parser.add_argument("ipaddr_spec", metavar="ipaddr_spec", type=str )
+args = parser.parse_args()
 
-main() {
-	arg_error=0
-	print_usage=0
-	raw=0
+def ip_to_int( ipaddr_str ):
+    parts = ipaddr_str.split( "." )
+    return (int( parts[0] ) << 24) + (int( parts[1] ) << 16) + (int( parts[2] ) << 8) + int( parts[3] )
 
-	# Check for raw mode, remove it from $@
-	# We require --raw as the first argument:
-	# - To avoid future clashes with "<UNDERLYING_UTILITY>" options
-	# - As we don't parse further input
-	# - As parsing further input would require knowing if an opt takes args
-	# $arg will get passed directly through to "<UNDERLYING_UTILITY>" later
-	if [ "$1" = '--raw' ] && [ "$raw_mode_support" -ne 0 ]; then
-		raw=1
-		shift
-	fi
+def int_to_ip( ipaddr_int ):
+    return str( ipaddr_int >> 24 ) + "." + str( (ipaddr_int & (255 << 16)) >> 16) + "." + str( (ipaddr_int & (255 << 8)) >> 8 ) + "."  + str( ipaddr_int & 255 )
 
-	# Raw mode
-	if [ "$raw" -eq 1 ]; then
-		if [ -z "$@" ]; then
-			echo 'Raw mode. No further opts/args provided.'
-		else
-			echo "Raw mode. Input provided: $@"
-		fi
+def network_ip( ipaddr, maskbits ):
+    int_ipaddr = ip_to_int( ipaddr )
+    mask = (int( "0xffffffff", 16 ) >> (32-int(maskbits))) << (32-int(maskbits))
+    return int_to_ip( int_ipaddr & mask )
 
-	# Normal mode
+ip_info = args.ipaddr_spec.split( "/" )
+
+# ip_info[0] is the ip addr, and ip_info[1] is the netmask bits
+print( network_ip( ip_info[0], ip_info[1] ) + "/" + ip_info[1] )
+    ' $1
+
+} # network_spec
+
+
+validate_nic()
+{
+
+    # Validate NICs
+    ip a | grep $1: &> /dev/null
+    if [ $? != 0 ]; then
+        errmsg "Error: Network interface $1 not found"
+        return 1
+    fi
+
+    ethtool $1 &> /dev/null
+    if [ $? != 0 ]; then
+        errmsg "Error: Network interface $1 not found"
+        return 1
+    fi
+
+    if [ `ethtool $1 | grep "Link detected:" | cut -d: -f2` != "yes" ]; then
+        errmsg "Error: Network interface $1 offline"
+        return 1
+    fi
+
+    INET=`ip address show dev $1 | grep "inet "`
+    if [ $? != 0 ]; then
+        errmsg "Error: Interface $1 has no ip address"
+        return 1
+    fi
+
+    echo "$INET" | awk '{print $2}'
+
+    return 0
+} # validate_nic
+
+
+###################################################
+# Main
+###################################################
+NET_SCRIPTS=/etc/sysconfig/network-scripts
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 nic_interface1 nic_interface2"
+    echo "or: $0 nic_interface1/gateway1 nic_interface2/gateway2"
+    echo "    example: $0 enp59s0f0 enp79s0f0"
+    echo "    optionally: $0 enp59s0f0/10.20.20.254 enp79s0f0/10.20.20.254"
+    exit 1
+else
+    NIC1=`echo $1 | awk -F/ '{ print $1 }'`
+    GATEWAY1=`echo $1 | awk -F/ '{ print $2 }'`
+    NIC2=`echo $2 | awk -F/ '{ print $1 }'`
+    GATEWAY2=`echo $2 | awk -F/ '{ print $2 }'`
+fi
+
+# check that the nic given are sane and fetch address info - X.X.X.X/nn
+NIC1_IP_SPEC=`validate_nic $NIC1`
+if [ $? != 0 ]; then
+    exit 1
+fi
+NIC2_IP_SPEC=`validate_nic $NIC2`
+if [ $? != 0 ]; then
+    exit 1
+fi
+
+
+# gather some details that we'll need
+NIC1_NET=`network_spec $NIC1_IP_SPEC`    # network number
+NIC2_NET=`network_spec $NIC2_IP_SPEC`
+
+NIC1_IP=`echo $NIC1_IP_SPEC | awk -F/ '{print $1}'`  # ip addr X.X.X.X
+NIC2_IP=`echo $NIC2_IP_SPEC | awk -F/ '{print $1}'`
+
+#echo "NIC1's IP: $NIC1_IP"
+#echo "NIC2's IP: $NIC2_IP"
+
+if [ "$NIC1_NET" != "$NIC2_NET" ]; then
+    errmsg "The two interfaces specified are not on the same network"
+    exit
+fi
+
+# unit test
+#exit
+
+# check for route scripts
+echo "Setting Route Scripts for $NIC1"
+if [ -f $NET_SCRIPTS/route-$NIC1 ]; then
+    errmsg "$NET_SCRIPTS/route-$NIC1 already exists.  Skipping"
+else
+    echo "$NIC1_NET dev $NIC1 src $NIC1_IP table weka1" > $NET_SCRIPTS/route-$NIC1
+    if [ "$GATEWAY1" != "" ]; then
+        echo "default via $GATEWAY1 dev $NIC1 table weka1" >> $NET_SCRIPTS/route-$NIC1
+    fi
+fi
+
+echo "Setting Route Scripts for $NIC2"
+if [ -f $NET_SCRIPTS/route-$NIC2 ]; then
+    errmsg "$NET_SCRIPTS/route-$NIC2 already exists.  Skipping"
+else
+    echo "$NIC2_NET dev $NIC2 src $NIC2_IP table weka2" > $NET_SCRIPTS/route-$NIC2
+    if [ "$GATEWAY2" != "" ]; then
+        echo "default via $GATEWAY2 dev $NIC2 table weka2" >> $NET_SCRIPTS/route-$NIC2
+    fi
+fi
+
+# check for rule scripts
+echo "Setting Rule Scripts for $NIC1"
+if [ -f $NET_SCRIPTS/rule-$NIC1 ]; then
+    errmsg "$NET_SCRIPTS/rule-$NIC1 already exists.  Skipping"
+else
+    echo "table weka1 from $NIC1_IP" > $NET_SCRIPTS/rule-$NIC1
+fi
+
+echo "Setting Rule Scripts for $NIC2"
+if [ -f $NET_SCRIPTS/rule-$NIC2 ]; then
+    errmsg "$NET_SCRIPTS/rule-$NIC2 already exists.  Skipping"
+else
+    echo "table weka2 from $NIC2_IP" > $NET_SCRIPTS/rule-$NIC2
+fi
+
+
+# check rt_tables
+echo "Setting Route Tables"
+grep weka1 /etc/iproute2/rt_tables &> /dev/null
+if [ $? != 1 ]; then
+    errmsg "/etc/iproute2/rt_tables seems to already have weka entries"
+else
+    echo "100 weka1" >> /etc/iproute2/rt_tables
+    echo "101 weka2" >> /etc/iproute2/rt_tables
+fi
+
+# check if entries are in /etc/sysctl.conf, if not, put them there.
+echo "Setting arp in sysctl.conf"
+for nic in "$NIC1" "$NIC2"; do
+	grep ^net.ipv4.conf.${nic}.arp_filter /etc/sysctl.conf &> /dev/null
+	if [ $? != 0 ]; then
+	    errmsg "Making entries in /etc/sysctl.conf"
+	    (
+	    echo "net.ipv4.conf.${nic}.arp_ignore = 0"
+	    echo "net.ipv4.conf.${nic}.arp_filter = 1"
+	    echo "net.ipv4.conf.${nic}.rp_filter = 2"
+	    echo "net.ipv4.conf.${nic}.arp_announce = 2"
+	    ) >> /etc/sysctl.conf
+	    sysctl -p /etc/sysctl.conf
 	else
-		# Convert long options to short options, stick them back on the end of $@
-		for arg in $@; do
-			case $arg in
-				--help) set -- "$@" '-h' ;;
-				--raw)
-					if [ "$raw_mode_support" -eq 0 ]; then
-						>&2 echo "$0: --raw must be first argument"
-					else
-						>&2 echo "$0: illegal long option -- ${arg:2}"
-					fi
-					arg_error=1
-					;;
-				--*)
-					>&2 echo "$0: illegal long option -- ${arg:2}"
-					arg_error=1
-					;;
-				*) set -- "$@" "$arg" ;;
-			esac
-			shift
-		done
-
-		eth=0 ib=0
-
-		# Cycle through options setting corresponding variables as options are
-		# found, or otherwise extract positional arguments
-		while [ "$OPTIND" -le "$#" ]; do
-			if getopts 'h' opts; then
-				case $opts in
-					h) print_usage=1 ;;
-					?) arg_error=1 ;;
-				esac
-			else
-				pos_args+=("${!OPTIND}")
-				((OPTIND++))
-			fi
-		done
-
-		# Set $@ to positional arguments only now that we have processed options
-		set -- ${pos_args[*]}
-		unset pos_args
-
-		if [ -z "$1" ] && [ "$print_usage" -ne 1 ]; then
-			>&2 echo "$0: no ports specified"
-			arg_error=1
-		fi
-
-		if [ "$arg_error" -ne 0 ]; then
-			[ "$print_usage_on_arg_err" -eq 1 ] && >&2 usage && exit 1
-			[ "$print_usage" -eq 1 ] && usage && exit 1
-			exit 1
-		fi
-
-		[ "$print_usage" -eq 1 ] && usage && exit 0
-
-		mst start || >&2 echo "$0: error starting MST, exiting"
-
-		# If not at least one argument; using $@ instead results in `binary
-		# operator expected` errors if an option is used (e.g. `-f`)
-		for port in $@; do
-			sysfs_port_path=$(readlink -f /sys/class/net/"$port"/device/)
-			port_pci_addr=$(basename "$sysfs_port_path")
-			port_pci_num=${port_pci_addr##*.}
-			port_num=$((port_pci_num + 1))
-
-			dev_pci_addr=${port_pci_addr%%.*}
-			device_file=$(mst status | grep -B 1 "$dev_pci_addr" | awk '{ print $1; exit }')
-
-			mlxconfig -d "$device_file" -y s ADVANCED_PCI_SETTINGS=1 PCI_WR_ORDERING=1
-			mlxfwreset -y -d "$device_file" reset
-		done
-
-		mst stop
+	    errmsg "Entries exist in /etc/sysctl.conf"
 	fi
-}
-
-main "$@"
-
-# vim: set filetype=sh:
+done
